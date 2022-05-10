10eb5a8e0637e7cb4d96d0d4d7b605cb4c8a7f76
Alessio Borraccino <alessio.borraccino@gmail.com>
2022-05-10 16:02:12 +0200

    Update bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -10,9 +10,14 @@ export default function Bio() {
             <AlexxEonNavbar></AlexxEonNavbar>
             <section className="section">
                 <div className="container is-half">
-                    <p className="has-text-centered has-text-primary-light" style={{ fontFamily: 'Anek Telugu', fontSize: 24, fontWeight: 400 }}>
+                    <p className="has-text-centered has-text-primary-light pt-2" style={{ fontFamily: 'Anek Telugu', fontSize: 24, fontWeight: 400 }}>
                         Music for Alexx was always there. <br />
-                        Since the beginning.
+                        Since the beginning. <br /> <br/>
+                        Born and raised in the heart of Italy, Alexx developed his love for music early on, when at 9 years old he starts taking piano lessons. <br/>
+                        While learning how to play and starting to understand how to write songs, he discovers the magic of electronic music, which ends up influencing his style. 
+                        He listened carefully to dance, trance, techno, hip hop, rnb and electronic music through the years, and gains interest in writing cover songs.<br/>
+                        <br/>
+                        He becomes later interested in singing in order to convey to his pieces a more personal feeling to it. Which takes us to the latest years, when Alexx decides to create a platform to get his covers out.  
                     </p>
                 </div>
             </section>
